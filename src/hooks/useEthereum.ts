de878cb0d242f979fa1ed1ae30030506edd3fabb
fibo <fibo@users.noreply.github.com>
2023-06-17 12:40:55 +0200

    hide tsbuild info

--- src/hooks/useEthereum.ts
+++ src/hooks/useEthereum.ts
@@ -1,4 +1,16 @@
+import detectEthereumProvider from "@metamask/detect-provider";
+import { useCallback, useEffect } from "react";
+
 export const useEthereum = () => {
+  const detectProvider = useCallback(async () => {
+    const provider = await detectEthereumProvider();
+    console.log(provider);
+  }, []);
+
+  useEffect(() => {
+    detectProvider();
+  }, []);
+
   let ethereumIsConnected: boolean | undefined = undefined;
   return {
     ethereumIsConnected,
