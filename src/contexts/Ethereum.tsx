44985d7519a5d03df355006091dbfe0f5639c3db
fibo <fibo@users.noreply.github.com>
2023-06-18 20:10:15 +0200

    use reducer to handle Ethereum state

--- src/contexts/Ethereum.tsx
+++ src/contexts/Ethereum.tsx
@@ -6,10 +6,34 @@ import {
   createContext,
   useCallback,
   useEffect,
-  useState,
+  useReducer,
+  Reducer,
 } from "react";
 
-type ContextValue = { isConnected: undefined | boolean };
+type State = Partial<{
+  chainId: string;
+  isConnected: boolean;
+  provider: MetaMaskInpageProvider | null;
+}>;
+
+type SET_CHAIN_ID = {
+  type: "SET_CHAIN_ID";
+  data: Pick<State, "chainId">;
+};
+
+type SET_IS_CONNECTED = {
+  type: "SET_IS_CONNECTED";
+  data: Pick<State, "isConnected">;
+};
+
+type SET_PROVIDER = {
+  type: "SET_PROVIDER";
+  data: Pick<State, "provider">;
+};
+
+type Action = SET_CHAIN_ID | SET_IS_CONNECTED | SET_PROVIDER;
+
+type ContextValue = Pick<State, "isConnected">;
 
 export const EthereumContext = createContext<ContextValue>({
   isConnected: undefined,
@@ -17,29 +41,77 @@ export const EthereumContext = createContext<ContextValue>({
 
 EthereumContext.displayName = "EthereumContext";
 
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "SET_CHAIN_ID": {
+      return {
+        ...state,
+        chainId: action.data.chainId,
+      };
+    }
+
+    case "SET_IS_CONNECTED": {
+      return {
+        ...state,
+        isConnected: action.data.isConnected,
+      };
+    }
+
+    case "SET_PROVIDER": {
+      return {
+        ...state,
+        provider: action.data.provider,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
 export const EthereumContextProvider: FC<PropsWithChildren> = ({
   children,
 }) => {
-  const [hasProvider, setHasProvider] = useState<boolean | undefined>();
-  const [isConnected, setIsConnected] = useState<boolean | undefined>();
-
-  let provider: MetaMaskInpageProvider | null = null;
+  const [{ chainId, isConnected, provider }, dispatch] = useReducer<
+    Reducer<State, Action>
+  >(reducer, {});
 
   const detectProvider = useCallback(async () => {
-    provider = await detectEthereumProvider<MetaMaskInpageProvider>();
-    if (provider) {
-      setHasProvider(true);
-      setIsConnected(provider.isConnected());
-    } else {
-      setIsConnected(false);
-      setHasProvider(false);
-    }
-  }, []);
+    if (provider) return;
+    const detectedProvider =
+      await detectEthereumProvider<MetaMaskInpageProvider>();
+    dispatch({
+      type: "SET_IS_CONNECTED",
+      data: {
+        isConnected:
+          detectedProvider === null ? false : detectedProvider.isConnected(),
+      },
+    });
+  }, [provider]);
+
+  const requestChainId = useCallback(async () => {
+    if (!provider) return;
+    const chainId = await provider.request<State["chainId"]>({
+      method: "eth_chainId",
+    });
+    if (!chainId) return;
+    dispatch({ type: "SET_CHAIN_ID", data: { chainId } });
+  }, [provider]);
 
   useEffect(() => {
-    if (hasProvider) return;
-    detectProvider();
-  }, [hasProvider]);
+    if (provider === undefined) {
+      detectProvider();
+    } else if (provider !== null) {
+      provider.on("chainChanged", () => {
+        window.location.reload();
+      });
+    }
+  }, [provider]);
+
+  useEffect(() => {
+    if (chainId) return;
+    requestChainId();
+  }, [chainId, requestChainId]);
 
   return (
     <EthereumContext.Provider value={{ isConnected }}>
