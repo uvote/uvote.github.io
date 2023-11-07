98ca48aa39bda047a167f84db11f891e8696c6f6
Gianluca Casati <fibo@users.noreply.github.com>
2023-11-07 21:00:20 +0000

    add "No network" message

--- src/components/library/Main.tsx
+++ src/components/library/Main.tsx
@@ -1,11 +1,24 @@
+import { useOfflineDetection } from "_/hooks/useOfflineDetection";
 import { classNames } from "_/styles/classNames";
 import { FC, PropsWithChildren } from "react";
-import { Container } from "trunx";
+import { FormattedMessage } from "react-intl";
+import { Container, Message } from "trunx";
 
 export const Main: FC<PropsWithChildren> = ({ children }) => {
+  const isOffline = useOfflineDetection();
+  console.log("isOffline", isOffline);
+
   return (
     <main className={classNames("Main__container", "px-2", "py-4")}>
-      <Container>{children}</Container>
+      <Container>
+        {isOffline ? (
+          <Message color="warning">
+            <FormattedMessage id="Main.noNetwork" />
+          </Message>
+        ) : null}
+
+        {children}
+      </Container>
     </main>
   );
 };
