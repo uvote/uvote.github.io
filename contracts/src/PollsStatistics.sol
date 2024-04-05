12a70b5436bde1927c236672908a6cdbe9383052
Gianluca Casati <fibo@users.noreply.github.com>
2024-04-05 10:48:52 +0200

    add OverallStatistics

--- contracts/src/PollsStatistics.sol
+++ contracts/src/PollsStatistics.sol
@@ -1,22 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+struct OverallStatistics {
+    uint32 totalNumberOfPolls;
+    uint32 uniqueVoters;
+}
+
 /// @title Polls statistics
 /// @notice It collects poll results and few basic metrics.
 /// @author Gianluca Casati https://fibo.github.io
 contract PollsStatistics {
     mapping(bytes32 => mapping(uint8 => uint32)) private pollResults;
-    mapping(bytes32 => bool) private seenVoter;
+    mapping(bytes32 => bool) private seenPoll;
+    mapping(address => bool) private seenVoter;
     mapping(bytes32 => mapping(bytes32 => bool)) private seenVoterByPoll;
 
+    // @notice The overall number of polls.
+    uint32 private totalNumberOfPolls;
+
     // @notice The overall number of votes.
-    uint64 totalNumberOfVotes;
+    uint64 private totalNumberOfVotes;
 
     // @notice The number of votes for given poll.
     mapping(bytes32 => uint32) private totalNumberOfVotesByPoll;
 
     // @notice The overall number of unique voters.
-    uint32 uniqueVoters;
+    uint32 private uniqueVoters;
 
     // @notice The number of unique voters for given poll.
     mapping(bytes32 => uint32) private uniqueVotersByPoll;
@@ -27,6 +36,10 @@ contract PollsStatistics {
 
     function getVoterKey(address pollFactory, uint32 pollId, address voter) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(pollFactory, pollId, voter));
+    }
+
+    function readOverallStatistics() external view returns (OverallStatistics memory) {
+        return OverallStatistics(totalNumberOfPolls, uniqueVoters);
     }
 
     function readPollResults(uint32 pollId, uint8 numChoices) external view returns (uint32[] memory) {
@@ -41,14 +54,19 @@ contract PollsStatistics {
     }
 
     function votePoll(uint32 pollId, address voter, uint8 choice) external {
-        bytes32 voterKey = getVoterKey(msg.sender, pollId, voter);
-
-        if (!seenVoter[voterKey]) {
-            seenVoter[voterKey] = true;
+        if (!seenVoter[voter]) {
+            seenVoter[voter] = true;
             uniqueVoters++;
         }
 
+        bytes32 voterKey = getVoterKey(msg.sender, pollId, voter);
+
         bytes32 pollKey = getPollKey(msg.sender, pollId);
+
+        if (!seenPoll[pollKey]) {
+            seenPoll[pollKey] = true;
+            totalNumberOfPolls++;
+        }
 
         if (!seenVoterByPoll[pollKey][voterKey]) {
             seenVoterByPoll[pollKey][voterKey] = true;
--- contracts/test/PollsStatistics.t.sol
+++ contracts/test/PollsStatistics.t.sol
@@ -11,20 +11,43 @@ contract PollsStatisticsTest is Test {
         pollsStatistics = new PollsStatistics();
     }
 
+    function test_readOverallStatistics_uniqueVoters() public {
+        address pollFactory = address(1);
+        address voterA = address(2);
+        address voterB = address(3);
+        address voterC = address(4);
+        uint8 choiceA = 1;
+        uint8 choiceB = 2;
+        uint8 choiceC = 3;
+        uint32 pollIdA = 42;
+        uint32 pollIdB = 43;
+        address statisticsConsumer = address(9);
+        vm.startPrank(pollFactory);
+        pollsStatistics.votePoll(pollIdA, voterA, choiceA);
+        pollsStatistics.votePoll(pollIdA, voterB, choiceB);
+        pollsStatistics.votePoll(pollIdB, voterA, choiceA);
+        pollsStatistics.votePoll(pollIdB, voterC, choiceC);
+        vm.stopPrank();
+        vm.prank(statisticsConsumer);
+        OverallStatistics memory overallStatistics = pollsStatistics.readOverallStatistics();
+        assertEq(overallStatistics.uniqueVoters, 3);
+        assertEq(overallStatistics.totalNumberOfPolls, 2);
+    }
+
     function test_votePoll_updates_poll_results() public {
         address pollFactory = address(1);
         address voterA = address(2);
         address voterB = address(3);
-        uint8 voteA = 0;
-        uint8 voteB = 1;
+        uint8 choiceA = 1;
+        uint8 choiceB = 2;
         uint32 pollId = 42;
-        uint8 numChoices = 2;
+        uint8 numChoices = 3;
         vm.startPrank(pollFactory);
-        pollsStatistics.votePoll(pollId, voterA, voteA);
-        pollsStatistics.votePoll(pollId, voterB, voteB);
+        pollsStatistics.votePoll(pollId, voterA, choiceA);
+        pollsStatistics.votePoll(pollId, voterB, choiceB);
         uint32[] memory pollResults = pollsStatistics.readPollResults(pollId, numChoices);
-        assertEq(pollResults[0], 1);
-        assertEq(pollResults[1], 1);
+        assertEq(pollResults[choiceA], 1);
+        assertEq(pollResults[choiceB], 1);
         vm.stopPrank();
     }
 }
