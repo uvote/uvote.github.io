18b2355fd3bedfeb69a3068faf1c87c00e373cb4
Gianluca Casati <fibo@users.noreply.github.com>
2024-04-08 19:19:08 +0200

    add blank votes

--- contracts/src/PollsStatistics.sol
+++ contracts/src/PollsStatistics.sol
@@ -5,56 +5,57 @@ struct PollStatistics {
     uint64 totalNumberOfVotes;
     uint64 numberOfValidVotes;
     uint64 numberOfBlankVotes;
-    uint32 uniqueVoters;
 }
 
-// @notice A voter cannot submit the same choice twice.
-error ErrorChoiceAlreadyExists();
+// @notice A voter cannot submit the same valid vote twice.
+error ErrorValidVoteAlreadyExists();
 
-// @notice A voter cannot dismiss a choice if it does not exist.
-error ErrorChoiceDoesNotExist();
+// @notice A voter cannot submit the same blank vote twice.
+error ErrorBlankVoteAlreadyExists();
+
+// @notice A voter cannot submit the a vote if he/she already voted blank.
+error ErrorVoterDidBlankVote();
+
+// @notice A voter cannot dismiss a valid vote if it does not exist.
+error ErrorValidVoteDoesNotExist();
+
+// @notice A voter cannot dismiss a blank vote if it does not exist.
+error ErrorBlankVoteDoesNotExist();
+
+// @notice A voter cannot submit a blank vote if he/she already did a valid vote.
+error ErrorVoterDidSomeValidVote();
 
 /// @title Polls statistics
 /// @notice It collects poll results and few basic metrics.
 /// @author Gianluca Casati https://fibo.github.io
 contract PollsStatistics {
-    mapping(bytes32 => mapping(uint8 => uint32)) private pollResults;
-    mapping(bytes32 => bool) private seenChoice;
-    mapping(bytes32 => bool) private seenVoter;
+    type PollKey is bytes32;
+    type VoterKey is bytes32;
 
-    // @notice The total number of votes by given poll, including blanks.
-    mapping(bytes32 => uint32) private totalNumberOfVotes;
+    mapping(PollKey => mapping(uint8 => uint32)) private pollResults;
+    mapping(VoterKey => bool) private seenBlankVote;
+    mapping(VoterKey => bool) private seenValidVote;
+
+    // @notice The total number of votes by given poll: it includes valid votes and blank votes.
+    mapping(PollKey => uint32) private totalNumberOfVotes;
 
     // @notice The number of valid votes by given poll.
-    mapping(bytes32 => uint32) private numberOfValidVotes;
+    mapping(PollKey => uint32) private numberOfValidVotes;
 
     // @notice The number of blank votes by given poll.
-    mapping(bytes32 => uint32) private numberOfBlankVotes;
+    mapping(PollKey => uint32) private numberOfBlankVotes;
 
-    // @notice The number of unique voters for given poll.
-    mapping(bytes32 => uint32) private uniqueVoters;
-
-    function getPollKey(address pollFactory, uint32 pollId) internal pure returns (bytes32) {
-        return keccak256(abi.encodePacked(pollFactory, pollId));
+    function getPollKey(address pollFactory, uint32 pollId) internal pure returns (PollKey) {
+        return PollKey.wrap(keccak256(abi.encodePacked(pollFactory, pollId)));
     }
 
-    function getVoterKey(address pollFactory, uint32 pollId, address voter) internal pure returns (bytes32) {
-        return keccak256(abi.encodePacked(pollFactory, pollId, voter));
-    }
-
-    function getChoiceKey(address pollFactory, uint32 pollId, address voter, uint8 choice)
-        internal
-        pure
-        returns (bytes32)
-    {
-        return keccak256(abi.encodePacked(pollFactory, pollId, voter, choice));
+    function getVoterKey(address pollFactory, uint32 pollId, address voter) internal pure returns (VoterKey) {
+        return VoterKey.wrap(keccak256(abi.encodePacked(pollFactory, pollId, voter)));
     }
 
     function readPollStatistics(address pollFactory, uint32 pollId) external view returns (PollStatistics memory) {
-        bytes32 pollKey = getPollKey(pollFactory, pollId);
-        return PollStatistics(
-            totalNumberOfVotes[pollKey], numberOfValidVotes[pollKey], numberOfBlankVotes[pollKey], uniqueVoters[pollKey]
-        );
+        PollKey pollKey = getPollKey(pollFactory, pollId);
+        return PollStatistics(totalNumberOfVotes[pollKey], numberOfValidVotes[pollKey], numberOfBlankVotes[pollKey]);
     }
 
     function readPollResults(address pollFactory, uint32 pollId, uint8 numChoices)
@@ -63,7 +64,7 @@ contract PollsStatistics {
         returns (uint32[] memory)
     {
         uint32[] memory results = new uint32[](numChoices);
-        bytes32 key = getPollKey(pollFactory, pollId);
+        PollKey key = getPollKey(pollFactory, pollId);
         uint8 i;
         while (i < numChoices) {
             results[i] = pollResults[key][i];
@@ -72,56 +73,89 @@ contract PollsStatistics {
         return results;
     }
 
+    // @notice A voter can vote her/his choice.
     // @dev The `pollFactory` here is the `msg.sender`.
     function vote(uint32 pollId, address voter, uint8 choice) external {
-        // Check that choice does not exist yet.
+        // Check that voter did not already choose a blank vote.
 
-        bytes32 choiceKey = getChoiceKey(msg.sender, pollId, voter, choice);
+        VoterKey voterKey = getVoterKey(msg.sender, pollId, voter);
 
-        if (seenChoice[choiceKey]) revert ErrorChoiceAlreadyExists();
+        if (seenBlankVote[voterKey]) revert ErrorVoterDidBlankVote();
+
+        // Check that valid vote does not exist yet.
+
+        if (seenValidVote[voterKey]) revert ErrorValidVoteAlreadyExists();
 
         // Increment poll results by given choice.
 
-        bytes32 pollKey = getPollKey(msg.sender, pollId);
+        PollKey pollKey = getPollKey(msg.sender, pollId);
 
         pollResults[pollKey][choice]++;
-        seenChoice[choiceKey] = true;
+        seenValidVote[voterKey] = true;
 
         // Increment number of votes on given poll.
 
         totalNumberOfVotes[pollKey]++;
         numberOfValidVotes[pollKey]++;
-
-        // Increment number of unique voters on given poll.
-
-        bytes32 voterKey = getVoterKey(msg.sender, pollId, voter);
-
-        if (!seenVoter[voterKey]) {
-            seenVoter[voterKey] = true;
-            uniqueVoters[pollKey]++;
-        }
     }
 
     // @notice A vote can be dismissed, in case voter changed her/his mind or voted by mistake.
     function dismissVote(uint32 pollId, address voter, uint8 choice) external {
-        // Check that choice already exists.
+        // Check that valid vote already exists.
 
-        bytes32 choiceKey = getChoiceKey(msg.sender, pollId, voter, choice);
+        VoterKey voterKey = getVoterKey(msg.sender, pollId, voter);
 
-        if (!seenChoice[choiceKey]) revert ErrorChoiceDoesNotExist();
+        if (!seenValidVote[voterKey]) revert ErrorValidVoteDoesNotExist();
 
         // Decrement poll results by given choice.
 
-        bytes32 pollKey = getPollKey(msg.sender, pollId);
+        PollKey pollKey = getPollKey(msg.sender, pollId);
 
         pollResults[pollKey][choice]--;
-        seenChoice[choiceKey] = false;
+        seenValidVote[voterKey] = false;
 
         // Decrement number of votes on given poll.
 
         totalNumberOfVotes[pollKey]--;
         numberOfValidVotes[pollKey]--;
+    }
 
-        // Number of unique voters does not change, as the voter interacted somehow with the poll.
+    // @notice A voter can choose to do a blank vote.
+    // @dev The `pollFactory` here is the `msg.sender`.
+    function blankVote(uint32 pollId, address voter) external {
+        // Check that voter did not already choose some valid vote.
+
+        VoterKey voterKey = getVoterKey(msg.sender, pollId, voter);
+
+        if (seenValidVote[voterKey]) revert ErrorVoterDidSomeValidVote();
+
+        // Check that blank vote does not exist yet.
+
+        if (seenBlankVote[voterKey]) revert ErrorBlankVoteAlreadyExists();
+
+        // Increment number of votes on given poll.
+
+        PollKey pollKey = getPollKey(msg.sender, pollId);
+
+        seenBlankVote[voterKey] = true;
+        totalNumberOfVotes[pollKey]++;
+        numberOfBlankVotes[pollKey]++;
+    }
+
+    // @notice A blank vote can be dismissed, in case voter changed her/his mind or voted by mistake.
+    function dismissBlankVote(uint32 pollId, address voter) external {
+        // Check that blank vote already exists.
+
+        VoterKey voterKey = getVoterKey(msg.sender, pollId, voter);
+
+        if (!seenBlankVote[voterKey]) revert ErrorBlankVoteDoesNotExist();
+
+        // Decrement number of votes on given poll.
+
+        PollKey pollKey = getPollKey(msg.sender, pollId);
+
+        seenBlankVote[voterKey] = false;
+        totalNumberOfVotes[pollKey]--;
+        numberOfBlankVotes[pollKey]--;
     }
 }
--- contracts/test/PollsStatistics.t.sol
+++ contracts/test/PollsStatistics.t.sol
@@ -11,6 +11,8 @@ contract PollsStatisticsTest is Test {
         pollsStatistics = new PollsStatistics();
     }
 
+    // readPollStatistics
+
     function test_readPollStatistics1() public {
         address pollFactory = address(1);
         address voterA = address(2);
@@ -30,7 +32,6 @@ contract PollsStatisticsTest is Test {
         assertEq(pollStatistics.totalNumberOfVotes, 3);
         assertEq(pollStatistics.numberOfValidVotes, 3);
         assertEq(pollStatistics.numberOfBlankVotes, 0);
-        assertEq(pollStatistics.uniqueVoters, 3);
     }
 
     function test_readPollStatistics2() public {
@@ -51,8 +52,52 @@ contract PollsStatisticsTest is Test {
         assertEq(pollStatistics.totalNumberOfVotes, 1);
         assertEq(pollStatistics.numberOfValidVotes, 1);
         assertEq(pollStatistics.numberOfBlankVotes, 0);
-        assertEq(pollStatistics.uniqueVoters, 2);
     }
+
+    function test_readPollStatistics3() public {
+        address pollFactory = address(1);
+        address voterA = address(2);
+        address voterB = address(3);
+        address voterC = address(4);
+        uint32 pollId = 42;
+        uint8 choiceA = 1;
+        uint8 choiceB = 2;
+        address statisticsConsumer = address(9);
+        vm.startPrank(pollFactory);
+        pollsStatistics.vote(pollId, voterA, choiceA);
+        pollsStatistics.vote(pollId, voterB, choiceB);
+        pollsStatistics.blankVote(pollId, voterC);
+        vm.stopPrank();
+        vm.prank(statisticsConsumer);
+        PollStatistics memory pollStatistics = pollsStatistics.readPollStatistics(pollFactory, pollId);
+        assertEq(pollStatistics.totalNumberOfVotes, 3);
+        assertEq(pollStatistics.numberOfValidVotes, 2);
+        assertEq(pollStatistics.numberOfBlankVotes, 1);
+    }
+
+    function test_readPollStatistics4() public {
+        address pollFactory = address(1);
+        address voterA = address(2);
+        address voterB = address(3);
+        address voterC = address(4);
+        uint32 pollId = 42;
+        uint8 choiceA = 1;
+        uint8 choiceB = 2;
+        address statisticsConsumer = address(9);
+        vm.startPrank(pollFactory);
+        pollsStatistics.vote(pollId, voterA, choiceA);
+        pollsStatistics.vote(pollId, voterB, choiceB);
+        pollsStatistics.blankVote(pollId, voterC);
+        pollsStatistics.dismissBlankVote(pollId, voterC);
+        vm.stopPrank();
+        vm.prank(statisticsConsumer);
+        PollStatistics memory pollStatistics = pollsStatistics.readPollStatistics(pollFactory, pollId);
+        assertEq(pollStatistics.totalNumberOfVotes, 2);
+        assertEq(pollStatistics.numberOfValidVotes, 2);
+        assertEq(pollStatistics.numberOfBlankVotes, 0);
+    }
+
+    // vote
 
     function test_vote_updates_poll_results() public {
         address pollFactory = address(1);
@@ -73,17 +118,32 @@ contract PollsStatisticsTest is Test {
         assertEq(pollResults[choiceB], 2);
     }
 
-    function test_vote_checks_choice_happens_once() public {
+    function test_vote_checks_voter_choices_once() public {
+        address pollFactory = address(1);
+        address voter = address(2);
+        uint32 pollId = 42;
+        uint8 choice1 = 1;
+        uint8 choice2 = 2;
+        vm.startPrank(pollFactory);
+        pollsStatistics.vote(pollId, voter, choice1);
+        vm.expectRevert(ErrorValidVoteAlreadyExists.selector);
+        pollsStatistics.vote(pollId, voter, choice2);
+        vm.stopPrank();
+    }
+
+    function test_vote_checks_voter_did_not_a_blank_vote() public {
         address pollFactory = address(1);
         address voter = address(2);
         uint32 pollId = 42;
         uint8 choice = 1;
         vm.startPrank(pollFactory);
-        pollsStatistics.vote(pollId, voter, choice);
-        vm.expectRevert(ErrorChoiceAlreadyExists.selector);
+        pollsStatistics.blankVote(pollId, voter);
+        vm.expectRevert(ErrorVoterDidBlankVote.selector);
         pollsStatistics.vote(pollId, voter, choice);
         vm.stopPrank();
     }
+
+    // dismissVote
 
     function test_dismissVote_checks_choice_exists1() public {
         address pollFactory = address(1);
@@ -91,7 +151,7 @@ contract PollsStatisticsTest is Test {
         uint32 pollId = 42;
         uint8 choice = 1;
         vm.startPrank(pollFactory);
-        vm.expectRevert(ErrorChoiceDoesNotExist.selector);
+        vm.expectRevert(ErrorValidVoteDoesNotExist.selector);
         pollsStatistics.dismissVote(pollId, voter, choice);
         vm.stopPrank();
     }
@@ -104,7 +164,7 @@ contract PollsStatisticsTest is Test {
         vm.startPrank(pollFactory);
         pollsStatistics.vote(pollId, voter, choice);
         pollsStatistics.dismissVote(pollId, voter, choice);
-        vm.expectRevert(ErrorChoiceDoesNotExist.selector);
+        vm.expectRevert(ErrorValidVoteDoesNotExist.selector);
         pollsStatistics.dismissVote(pollId, voter, choice);
         vm.stopPrank();
     }
@@ -125,5 +185,54 @@ contract PollsStatisticsTest is Test {
         uint32[] memory pollResults = pollsStatistics.readPollResults(pollFactory, pollId, numChoices);
         assertEq(pollResults[choiceA], 0);
         assertEq(pollResults[choiceB], 1);
+    }
+
+    // blankVote
+
+    function test_blankVote_checks_choice_happens_once() public {
+        address pollFactory = address(1);
+        address voter = address(2);
+        uint32 pollId = 42;
+        vm.startPrank(pollFactory);
+        pollsStatistics.blankVote(pollId, voter);
+        vm.expectRevert(ErrorBlankVoteAlreadyExists.selector);
+        pollsStatistics.blankVote(pollId, voter);
+        vm.stopPrank();
+    }
+
+    function test_blankVote_checks_voter_did_not_choose_some_valid_vote() public {
+        address pollFactory = address(1);
+        address voter = address(2);
+        uint32 pollId = 42;
+        uint8 choice = 1;
+        vm.startPrank(pollFactory);
+        pollsStatistics.vote(pollId, voter, choice);
+        vm.expectRevert(ErrorVoterDidSomeValidVote.selector);
+        pollsStatistics.blankVote(pollId, voter);
+        vm.stopPrank();
+    }
+
+    // dismissBlankVote
+
+    function test_dismissBlankVote_checks_blank_vote_exists1() public {
+        address pollFactory = address(1);
+        address voter = address(2);
+        uint32 pollId = 42;
+        vm.startPrank(pollFactory);
+        vm.expectRevert(ErrorBlankVoteDoesNotExist.selector);
+        pollsStatistics.dismissBlankVote(pollId, voter);
+        vm.stopPrank();
+    }
+
+    function test_dismissBlankVote_checks_blank_vote_exists2() public {
+        address pollFactory = address(1);
+        address voter = address(2);
+        uint32 pollId = 42;
+        vm.startPrank(pollFactory);
+        pollsStatistics.blankVote(pollId, voter);
+        pollsStatistics.dismissBlankVote(pollId, voter);
+        vm.expectRevert(ErrorBlankVoteDoesNotExist.selector);
+        pollsStatistics.dismissBlankVote(pollId, voter);
+        vm.stopPrank();
     }
 }
