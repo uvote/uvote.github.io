87225b2088511d0e7032617bff005f1a3f59cb4e
fibo <fibo@users.noreply.github.com>
2024-04-12 22:25:23 +0200

    MVP can readPollResults

--- contracts/src/PollFactoryMVP.sol
+++ contracts/src/PollFactoryMVP.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.13;
 import "./PollsStatistics.sol";
 import "./PollsRegistry.sol";
 
-// @notice A poll has a title and two choices.
+uint8 constant INDEX_OF_CHOICE_A = 1;
+uint8 constant INDEX_OF_CHOICE_B = 2;
+
+// @notice A poll is expressed as a title and two choices.
 struct PollDetails {
     string title;
     string choiceA;
@@ -15,20 +18,22 @@ struct PollDetails {
 /// @notice It implements the simplest poll factory for uVote.
 /// @author Gianluca Casati https://fibo.github.io
 contract PollFactoryMVP is PollsRegistry, PollsStatistics {
-    mapping(uint256 => string) private titleOfPoll;
-    mapping(uint256 => string) private choiceAOfPoll;
-    mapping(uint256 => string) private choiceBOfPoll;
+    uint8 immutable NUM_CHOICES = 2;
+
+    mapping(uint256 => PollDetails) private detailsOfPoll;
 
     // @dev The poll creator here is the `msg.sender`.
     function createPoll(string memory title, string memory choiceA, string memory choiceB) public returns (uint256) {
         uint256 pollId = registerPoll(msg.sender);
-        titleOfPoll[pollId] = title;
-        choiceAOfPoll[pollId] = choiceA;
-        choiceBOfPoll[pollId] = choiceB;
+        detailsOfPoll[pollId] = PollDetails(title, choiceA, choiceB);
         return pollId;
     }
 
     function readPollDetails(uint256 pollId) public view returns (PollDetails memory) {
-        return PollDetails(titleOfPoll[pollId], choiceAOfPoll[pollId], choiceBOfPoll[pollId]);
+        return detailsOfPoll[pollId];
+    }
+
+    function readPollResults(uint256 pollId) public view returns (uint256[] memory) {
+        return this.readPollResults(pollId, NUM_CHOICES);
     }
 }
