3a7c8382dff5ccebd69d8341ca64948d4716a454
Gianluca Casati <fibo@users.noreply.github.com>
2024-04-28 15:48:40 +0200

    use vite development MODE and env vars

--- src/wagmi/config.ts
+++ src/wagmi/config.ts
@@ -1,13 +1,14 @@
 import { createConfig, http } from "wagmi";
-import { goerli, mainnet } from "wagmi/chains";
+import { foundry, goerli, mainnet } from "wagmi/chains";
 import { coinbaseWallet, injected } from "wagmi/connectors";
 
 import * as metadata from "../metadata.json";
 
 const appName = metadata.asciiName;
+const RPC_URL = import.meta.env.VITE_RPC_URL;
 
 export const wagmiConfig = createConfig({
-  chains: [mainnet, goerli],
+  chains: [foundry, goerli, mainnet],
   connectors: [
     injected(),
     coinbaseWallet({
@@ -15,7 +16,8 @@ export const wagmiConfig = createConfig({
     }),
   ],
   transports: {
-    [mainnet.id]: http(),
-    [goerli.id]: http(),
+    [foundry.id]: http(),
+    [goerli.id]: http(RPC_URL),
+    [mainnet.id]: http(RPC_URL),
   },
 });
